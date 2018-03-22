60b0f91c7879edf0a227d61b747e8e752ab0aeb1
Patrick Leee <patrickleee@foxmail.com>
2018-03-22 10:41:15 +0800

    the fucking cross-domain access

--- src/main/java/com/bigdata/config/CorsFilter.java
+++ src/main/java/com/bigdata/config/CorsFilter.java
@@ -3,6 +3,7 @@ package com.bigdata.config;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -22,12 +23,37 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res,
                          FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header, Authorization");
-        chain.doFilter(req, res);
+        response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        response.setHeader("Content-type", "text/html;charset=UTF-8");
+
+        String uri = request.getRequestURI().substring(request.getContextPath().length());
+        // 登录、身份校验时，直接请求
+        if (request.getRequestURI().contains("/cors") || request.getRequestURI().contains("/login") || request.getRequestURI().contains("oauth/token")) {
+            chain.doFilter(req, res);
+        } else if (request.getRequestURI().contains("/rest")) {// 浏览器的非登录请求，统一进行跨域处理
+
+            String method = request.getMethod();
+            String realUri = uri.substring(5);
+
+            String authorization = request.getHeader("Authorization") == null ? "" : request.getHeader("Authorization");
+
+            // 判断是否跨域的请求
+            if ("OPTIONS".equals(method) || !authorization.startsWith("bearer ")) {
+                RequestDispatcher rd = request.getRequestDispatcher("/cors");
+                rd.forward(request, response);
+            } else {
+                RequestDispatcher rd = request.getRequestDispatcher(realUri);
+                rd.forward(request, response);
+            }
+        } else {// 跨域请求转发，直接请求
+            chain.doFilter(req, res);
+        }
     }
 
     @Override
